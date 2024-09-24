3f610ee80bb87df91c0bbd9562efbf75d8478e25
A Samuel Pottinger <sam@gleap.org>
2024-09-24 22:06:17 +0000

    Finish initial code editor.

---
+++ js/results.js
@@ -0,0 +1,22 @@
+class ResultsPresenter {
+
+  constructor(targetId) {
+    const self = this;
+    self._root = document.getElementById(targetId);
+    self.hide();
+  }
+
+  hide() {
+    const self = this;
+    self._root.style.display = "none";
+  }
+
+  showResults() {
+    const self = this;
+    self._root.style.display = "block";
+  }
+
+}
+
+
+export { ResultsPresenter };
--- support/install_deps.sh
+++ support/install_deps.sh
@@ -4,3 +4,16 @@
 [ ! -e third_party ] && mkdir third_party
 
 [ ! -e third_party/d3.min.js ] && wget https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js -O third_party/d3.min.js
+
+[ ! -e third_party/publicsans ] && mkdir third_party/publicsans
+
+[ ! -e third_party/publicsans/public-sans-v2.001.zip ] && wget https://github.com/uswds/public-sans/releases/download/v2.001/public-sans-v2.001.zip -O third_party/publicsans/public-sans-v2.001.zip
+
+if [ ! -e third_party/publicsans/fonts/otf/PublicSans-Regular.otf ]; then
+  cd third_party/publicsans
+  unzip public-sans-v2.001.zip
+  cd ../..
+fi
+
+[ ! -e third_party/ace.min.js ] && wget https://cdnjs.cloudflare.com/ajax/libs/ace/1.32.3/ace.min.js -O third_party/ace.min.js
+[ ! -e third_party/theme-textmate.js ] && wget https://cdnjs.cloudflare.com/ajax/libs/ace/1.32.3/theme-textmate.min.js -O third_party/theme-textmate.js
