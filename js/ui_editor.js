9bbe8c5464d7747df6687c9ae6fe84b04bca6b99
A Samuel Pottinger <sam@gleap.org>
2024-09-30 22:04:22 +0000

    Add add dialog open.

--- js/ui_editor.js
+++ js/ui_editor.js
@@ -1,9 +1,31 @@
+class ApplicationsListPresenter {
+
+  constructor(targetId, dialogId) {
+    const self = this;
+    self._root = document.getElementById(targetId);
+    self._dialog = document.getElementById(dialogId);
+    self._setupAddLink();
+  }
+
+  _setupAddLink() {
+    const self = this;
+    const addLink = self._root.querySelector(".add-link");
+    addLink.addEventListener("click", (event) => {
+      self._dialog.show();
+      event.preventDefault();
+    });
+  }
+
+}
+
+
 class UiEditorPresenter {
 
   constructor(tabsId, contentsId) {
     const self = this;
     self._tabs = new Tabby("#" + tabsId);
     self._contentsSelection = document.getElementById(contentsId);
+    self._applicationsList = new ApplicationsListPresenter("ui-application-editor", "application-dialog");
     self._setupAdvancedLinks();
   }
 
