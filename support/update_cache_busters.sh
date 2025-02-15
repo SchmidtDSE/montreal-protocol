b078675f4f200890673af4dc6412c7d8ab6994fb
A Samuel Pottinger <sam@gleap.org>
2025-02-15 17:44:46 +0000

    Create separate check for cache busters.

--- support/update_cache_busters.sh
+++ support/update_cache_busters.sh
@@ -1,3 +1,9 @@
 epoch=$(date +%s)
-sed -i "s/EPOCH/${epoch}/g" deploy/index.html
-sed -i "s/EPOCH/${epoch}/g" guide/*.html
+
+if ! sed -i "s/EPOCH/${epoch}/g" deploy/guide/*.html; then
+    exit 1
+fi
+
+if ! sed -i "s/EPOCH/${epoch}/g" deploy/index.html; then
+    exit 2
+fi
