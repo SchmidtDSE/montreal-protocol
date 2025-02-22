8cd40b7d38767ff32cc78e434bfb976d032066e5
A Samuel Pottinger <sam@gleap.org>
2025-02-22 17:02:26 +0000

    Add test for units stability for kwh.

--- test/test_compiler.js
+++ test/test_compiler.js
@@ -75,6 +75,15 @@ function buildCompilerTests() {
       },
     ]);
 
+    buildTest("tests kwh units", "/test/qta/basic_kwh_units.qta", [
+      (result, assert) => {
+        const record = getResult(result, BAU_NAME, 1, 0, "test", "test");
+        const energyConsumption = record.getEnergyConsumption();
+        assert.closeTo(energyConsumption.getValue(), 500, 0.0001);
+        assert.deepEqual(energyConsumption.getUnits(), "kwh");
+      },
+    ]);
+
     buildTest("runs a basic script", "/test/qta/basic.qta", [
       (result, assert) => {
         const record = getResult(result, BAU_NAME, 1, 0, "test", "test");
