760b960a8b63b13b95d1517ed4361923d242924e
Sam Pottinger <sam@gleap.org>
2025-06-25 12:01:39 -0700

    One more cache buster.

--- support/update_cache_busters.sh
+++ support/update_cache_busters.sh
@@ -11,3 +11,11 @@ fi
 if ! sed -i "s/EPOCH/${epoch}/g" deploy/privacy.html; then
     exit 2
 fi
+
+if ! sed -i "s/EPOCH/${epoch}/g" deploy/js/wasm.worker.js; then
+    exit 3
+fi
+
+if ! sed -i "s/EPOCH/${epoch}/g" deploy/js/wasm_backend.js; then
+    exit 4
+fi
