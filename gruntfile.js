aa66a77e9bb399c9be5bc6ec50954576197baaac
Sam Pottinger <sam.pottinger@berkeley.edu>
2025-06-26 18:37:02 +0000

    Try sandbox first then fall back.

--- gruntfile.js
+++ gruntfile.js
@@ -12,7 +12,16 @@ module.exports = function (grunt) {
       },
     },
     qunit: {
-      all: {
+      withSandbox: {
+        options: {
+          urls: ["http://localhost:8000/test/test.html"],
+          timeout: 60000,
+          puppeteer: {
+            args: [],
+          },
+        },
+      },
+      withoutSandbox: {
         options: {
           urls: ["http://localhost:8000/test/test.html"],
           timeout: 60000,
@@ -24,5 +33,27 @@ module.exports = function (grunt) {
     },
   });
 
-  grunt.registerTask("default", ["connect", "qunit"]);
+  grunt.registerTask("qunit-with-fallback", function() {
+    var done = this.async();
+    
+    grunt.log.writeln("Attempting to run Chrome with sandbox enabled...");
+    
+    grunt.util.spawn({
+      grunt: true,
+      args: ["qunit:withSandbox"],
+    }, function(error, result, code) {
+      if (error || code !== 0) {
+        grunt.log.warn("Chrome sandbox failed. Falling back to --no-sandbox mode.");
+        grunt.log.warn("This may reduce security. Consider running in a proper environment with sandbox support.");
+        
+        grunt.task.run("qunit:withoutSandbox");
+        done();
+      } else {
+        grunt.log.ok("Chrome ran successfully with sandbox enabled.");
+        done();
+      }
+    });
+  });
+
+  grunt.registerTask("default", ["connect", "qunit-with-fallback"]);
 };
